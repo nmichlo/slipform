943a0d747c02749758aff757aebd55c9abdf04df
Nathan Michlo <NathanJMichlo@gmail.com>
2021-01-08 21:11:29 +0200

    pypi project

---
+++ slipform/__init__.py
@@ -0,0 +1,11 @@
+"""
+# Slipform
+pythonflow decorator for generating dataflow graphs using natural syntax.
+https://github.com/nmichlo/slipform
+"""
+
+__version__ = "0.0.0-alpha0"
+
+
+def slipfrom(func):
+    raise NotImplementedError('Slipfrom is still being developed')
